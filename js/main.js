a293b5c6cf5009e3873802d83bd0799884c2338e
Brendan Abolivier <contact@brendanabolivier.com>
2016-06-24 21:05:46 +0200

    The loader now waits for all the images to be loaded

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(loaded);
+$(window).on("load", loaded);
 
 // When working on the portfolio, use this function to skip the loader
 //$(document).ready(skipLoading);
@@ -7,7 +7,7 @@ $(document).ready(loaded);
 // on a link in the navbar
 $(document).on("click", "#navbar a", function() {
 	$('html, body').animate(
-        { 
+        {
             scrollTop: $($(this).attr("href")).offset().top - 100
         }
     , 750);
